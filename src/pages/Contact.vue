32ad50e1c12388650628acca718d1fdee5ab2b72
François CORNUAUD <francois.cornuaud@gmail.com>
2019-06-16 04:58:43 +0200

    Modifs pages contact

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,45 +1,45 @@
 <template>
-  <q-page class="home column justify-start items-center q-pa-md">
+  <q-page class="column justify-start items-center q-pa-md">
     <img src="assets/logo-mdp.svg?1.1.1" alt="Vincent GRATEAU"
          class="img-responsive">
-    <div class="row">
+    <div class="row justify-center">
       <div class="push-agency-content col-sm-7 col-md-5 col-lg-5">
         <h1>
-          <p class="push-agency-your-agency">Votre agence
-            <strong>Mutuelle de Poitiers Assurances</strong>
+          <p class="push-agency-your-agency">Votre agence<br/>
+            <strong>Mutuelle de Poitiers Assurances</strong> <br/>
+            à
           </p>
-          <span class="push-agency-city"> à POITIERS</span>
+          <span class="push-agency-city">POITIERS</span>
         </h1>
         <p>
           <strong>Vincent GRATEAU</strong>
           <br>Agent Général d'Assurance Exclusif
         </p>
-
         <p class="push-agency-phone">
-          <a href="tel:0549889664"><q-icon size="2em" name="phone"/> 05 49 88 96 64</a>
+          <a href="tel:0549889664">
+            <q-icon size="2em" name="phone"/>
+            <span style="display: inline-block; width: 140px;">05 49 88 96 64</span></a>
         </p>
 
         <p class="push-agency-email">
                         <span class="sr-only">Email
                             :</span>
-          <a href="mailto:agence.vincent.grateau@mutuelledepoitiers.fr"><q-icon size="2em" name="email"/> Email de l'agence</a>
+          <a href="mailto:agence.vincent.grateau@mutuelledepoitiers.fr">
+            <q-icon size="2em" name="email"/>
+            <span style="display: inline-block; width: 140px;">Email de l'agence</span></a>
         </p>
 
         <div class="push-agency-orias">
-                            <span>
-                                <a href="javascript:void(0);" role="button" class="btn btn-help" data-toggle="tooltip"
-                                   title="" data-original-title="www.orias.fr"><i class="fa fa-info"></i></a>
-                                N° Orias :
-                                                                    07007064
-                                                            </span>
+            <span>
+               <a href="javascript:void(0);" role="button" class="btn btn-help" data-toggle="tooltip"
+                  title="" data-original-title="www.orias.fr"><i class="fa fa-info"></i></a>
+               <span style="display: inline-block; width: 140px;">N° Orias :
+              07007064</span>
+            </span>
         </div>
 
       </div>
-      <div class="push-agency-hours col-sm-5 col-md-4 col-lg-4">
-        <div class="business-hours">
-          <span class="business-hours-close">Actuellement fermée</span>
-        </div>
-
+      <div class="push-agency-hours col-sm-5 col-md-4 col-lg-4" style="margin-top: 25px">
         <p class="text-uppercase"><strong>Horaires</strong></p>
 
         <div class="push-agency-days">
@@ -108,7 +108,7 @@
         </div>
 
         <p class="text-uppercase"><strong>Coordonnées</strong></p>
-        <address>
+        <address style="margin-bottom: 15px;">
           9 RUE DU MARCHE NOTRE DAME
           <br><strong>86000 POITIERS</strong>
         </address>
@@ -119,7 +119,7 @@
 </template>
 
 <style>
-  a{
+  a {
     text-decoration: none;
     color: #6ac025;
   }
@@ -130,3 +130,45 @@
     name: 'Contact'
   }
 </script>
+
+<style scoped lang="stylus">
+  .push-agency-content
+    padding-top 10px
+    text-align center
+
+    .push-agency-city
+      font-size 1.5em !important
+      line-height 100%
+      text-transform uppercase
+      font-weight bold
+      padding 0
+      margin 10px 0
+
+  h1
+    font-weight normal
+    padding: 10px 0;
+
+    .push-agency-your-agency
+      font-size: 0.75em !important;
+      line-height: 100%;
+
+  .push-agency-hours {
+    text-align: center;
+  }
+  ul:not(.list-puce), ol:not(.list-puce) {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  ul.push-agency-days {
+    display: inline-block;
+    text-align: left;
+    margin: 0 auto 25px;
+  }
+  ul.push-agency-days li span.day {
+    display: inline-block;
+    width: 90px;
+    text-align: left;
+    font-size: 1.125em !important;
+  }
+</style>
